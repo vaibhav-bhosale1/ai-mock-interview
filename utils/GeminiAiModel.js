e78bd7806aae31483822d5451a2405f691a6d5cd
Vaibhav Shivaji Bhosale <textmevaibhav@gmail.com>
2025-11-24 19:16:15 +0530

    update model version to gemini-2.5-flash

--- utils/GeminiAiModel.js
+++ utils/GeminiAiModel.js
@@ -8,7 +8,7 @@ const {
   const genAI = new GoogleGenerativeAI(apiKey);
   
   const model = genAI.getGenerativeModel({
-    model: "gemini-2.0-flash-exp",
+    model: "gemini-2.5-flash",
   });
   
   const generationConfig = {

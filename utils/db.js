de81e68dfa8c8342bc9e5ef9a4b76d3df5f38007
Vaibhav Shivaji Bhosale <165457598+vaibhav-bhosale1@users.noreply.github.com>
2026-04-03 08:59:46 +0530

    Change environment variable for database URL

--- utils/db.js
+++ utils/db.js
@@ -5,5 +5,5 @@ import * as schema from './schema'
 
 config({ path: ".env.local" }); // or .env.local
 
-const sql = neon(process.env.NEXT_PUBLIC_DRIZZLE_DB_URL);
+const sql = neon(process.env.DRIZZLE_DB_URL);
 export const db = drizzle( sql,{schema});
